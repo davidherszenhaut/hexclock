96e3d2b6f9ebfd55bc8f3425e27f15642c6eabd5
David Herszenhaut <davidherszenhaut@users.noreply.github.com>
2016-08-11 20:34:37 -0400

    Began stylizing hex color, added social anchors

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 function refreshData()
+
 {
     x = 1;
  	var d = new Date()
@@ -6,16 +7,16 @@ function refreshData()
  	var m = d.getMinutes();
  	var s = d.getSeconds();
  	
- 	if (h<=9) {h = '0'+h};
- 	if (m<=9) {m = '0'+m};
-	if (s<=9) {s = '0'+s};
+ 	if (h <= 9) {h = "0"+h};
+ 	if (m <= 9) {m = "0"+m};
+	if (s <= 9) {s = "0"+s};
 	
- 	var	color = '#'+h+m+s;
+ 	var	color = "#" + h + m + s;
  	
-    $("div.background").css("background-color", color );
+    $("div.wrapper").css("background-color", color );
     $("p#hex").text(color);
      
-    setTimeout(refreshData, x*1000);
+    setTimeout(refreshData, x * 1000);
 }
   
 refreshData();
